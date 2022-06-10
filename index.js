aa34d6e41e1be83d6a32bb4fda4b66bb811018d3
Patrick Bozic <patrickbozic@protonmail.com>
2022-06-10 22:44:55 +0800

    Asteroid Audio

--- index.js
+++ index.js
@@ -63,6 +63,7 @@ export default () => {
     move() {
       if(this.mesh.position.x > 300) {
         this.mesh.position.setX(-300);
+        console.log(this.sound.getVolume());
       }
       this.mesh.position.setX(this.mesh.position.x + this.velocityX);
       this.sound.position.copy(this.mesh.position);
