ed2c3f2541f326268039bb57064ecdca039ac7e0
Patrick Bozic <patrickbozic@protonmail.com>
2022-06-10 23:56:51 +0800

    Asteroid Audio

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ export default () => {
       this.sound = new THREE.PositionalAudio(audioListener);
       this.sound.setBuffer(soundBuffer);
       this.sound.setLoop(true);
-      this.sound.setRefDistance( 20 );
+      this.sound.setRefDistance( 10 );
       //this.sound.setMaxDistance( 1 );
       // this.sound.setDistanceModel('exponential');
       this.sound.play();
