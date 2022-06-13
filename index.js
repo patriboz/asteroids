2a2ee51e14c669332f88beb4589ee7b49082c133
Patrick Bozic <patrickbozic@protonmail.com>
2022-06-13 11:57:42 +0800

    Asteroid dust

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+// import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils.js';
 import {
   Bezier, ColorOverLife, ColorRange,
   ConeEmitter,DonutEmitter, ConstantColor, ConstantValue, FrameOverLife,
